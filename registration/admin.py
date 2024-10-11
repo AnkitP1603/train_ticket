c2c31b3316b1cfa635e1f7d4bbdcd5e54862d059
Fortius <ankitparkhi16@gmail.com>
2024-10-11 13:33:30 +0530

    Minor changes

--- registration/admin.py
+++ registration/admin.py
@@ -1,14 +1,4 @@
 from django.contrib import admin
-#from .models import Booking, Schedule
-
-# admin.site.register(Schedule)
-
-# @admin.register(Booking)
-# class BookingAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'train_name', 'start_station', 'dest_station', 'pnr', 'booking_status')
-#     search_fields = ('user__username', 'train_name', 'pnr')
-#     list_filter = ('booking_status', 'user')
-
 from .models import Passenger, Station, Train, TrainCarriage, Journey, Route, Seat, SeatBooking, Booking
 
 admin.site.register(Passenger)
