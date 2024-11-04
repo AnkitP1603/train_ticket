de687144a544634ba6c99fdce766c02675887ec9
Fortius <ankitparkhi16@gmail.com>
2024-11-04 19:23:27 +0530

    age field in profile

--- registration/signals.py
+++ registration/signals.py
@@ -3,16 +3,7 @@ from django.dispatch import receiver
 from decimal import Decimal
 from django.contrib.auth.models import User
 from .models import Route,Journey,Passenger,Seat,TrainCarriage
-
-@receiver(post_save, sender=User)
-def create_passenger(sender, instance, created, **kwargs):
-    if created:
-        Passenger.objects.create(
-            user=instance, 
-            passenger_name=instance.username, 
-            email=instance.email, 
-            age=0  
-        )
+from users.models import Profile
 
 
 @receiver(post_save, sender=Route)
